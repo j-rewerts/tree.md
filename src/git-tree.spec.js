3662dc2784a3263744f5b603828bce84a267a95e
j-rewerts <jaredrewerts@gmail.com>
2019-11-14 11:26:24 -0700

    Added repo clone tests.

--- src/git-tree.spec.js
+++ src/git-tree.spec.js
@@ -8,13 +8,24 @@ describe('GitTree', function() {
   })
   describe('_gitClone', function() {
     it('passes easily', function() {
-      assert(true, true)
+      assert.equal(true, true)
     })
 
     it('should clone a basic repo', function() {
       return tree._gitClone('https://github.com/j-rewerts/tree.md').then(path => {
-        assert(path, 'tree.md')
+        return assert.equal(path, 'tree.md')
       })
+    })
+
+    it('should clone a basic repo into a specific folder', function() {
+      let folder = 'abcd'
+      return tree._gitClone('https://github.com/j-rewerts/tree.md', folder).then(path => {
+        return assert.equal(path, folder)
+      })
+    })
+
+    it('should fail if repo does not exist', function() {
+      return assert.rejects(tree._gitClone('https://github.com/j-rewerts/dne'))
     })
   })
 })
