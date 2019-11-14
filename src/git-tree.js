06ad8ad4be23d8e33240cc6a2b51614535e827b0
j-rewerts <jaredrewerts@gmail.com>
2019-11-14 10:50:49 -0700

    Added folder path info when cloning.

--- src/git-tree.js
+++ src/git-tree.js
@@ -6,18 +6,38 @@ class GitTree {
     if (exec) {
       this._exec = exec
     }
+    // This RegExp helps match urls.
+    // For GitHub urls, capturing groups from string.match are as follows:
+    // 0: The matched string
+    // 1: Host (github.com)
+    // 2: Repo owner/organization (j-rewerts)
+    // 3: Repo name (tree.md)
+    // 4 (might not exist): Path (src/stuff)
+    this._urlRegExp = new RegExp('https?:\/\/([-a-zA-Z0-9@:%._\+~#=]{1,256})+\/([a-zA-Z-_.0-9]+)[\/#](.*)')
   }
 
-  getTree = (url, options) => {
+  getTree(url, options) {
     let folder = this._gitClone(url)
     return this._gitTree(folder)
   }
 
-  _gitClone = (url, folder) => {
-    return this._exec(`git clone ${url}`)
+  /**
+   * Clones a repo by url. Optionally clones into a named folder.
+   * @param {String} url The https Git url to clone.
+   * @param {String} [folder] The folder to clone into.
+   * @return {Promise} A promise that resolves with the folder name.
+   */
+  _gitClone(url, folder) {
+    if (!folder) {
+      folder = url.match(this._urlRegExp)[3]
+    }
+    
+    return this._exec(`git clone ${url} ${folder}`).then(() => {
+      return folder
+    })
   }
 
-  _gitTree = (path) => {
+  _gitTree(path) {
     return `
     README.md
     this_is_an_example.md
@@ -27,8 +47,8 @@ class GitTree {
     `
   }
 
-  _exec = (command) => {
-    return Promise((resolve, reject) => {
+  _exec(command) {
+    return new Promise((resolve, reject) => {
       // For some reason, stderr contains git's output.
       exec(command, (error, stdout, stderr) => {
         if (error) {
