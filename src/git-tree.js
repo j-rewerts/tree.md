f0ea671338636d70aeb1b119d39b5c2da6d10fc3
j-rewerts <jaredrewerts@gmail.com>
2019-11-13 20:57:01 -0700

    Inject exec into class.

--- src/git-tree.js
+++ src/git-tree.js
@@ -1,34 +1,44 @@
 const exec = require('child_process').exec
 
 
-module.exports = {
-  getTree: function(url, options) {
+class GitTree {
+  constructor(exec) {
+    if (exec) {
+      this._exec = exec
+    }
+  }
+
+  getTree = (url, options) => {
     let folder = this._gitClone(url)
     return this._gitTree(folder)
-  },
-  _gitClone: function(url, folder) {
-    let p = new Promise((resolve, reject) => {
-      // For some reason, stderr contains git's output.
-      exec(`git clone ${url}`, (error, stdout, stderr) => {
-        if (error) {
-          reject(error.message)
-        }
-        if (stderr) {
-          console.error(stderr)
-        }
+  }
 
-        resolve('path/to/folder')
-      })
-    })
-    return p
-  },
-  _gitTree: function(path) {
+  _gitClone = (url, folder) => {
+    return this._exec(`git clone ${url}`)
+  }
+
+  _gitTree = (path) => {
     return `
     README.md
     this_is_an_example.md
     fldr
-     |
-     ___ anotherFile.js
+    |
+    ___ anotherFile.js
     `
   }
+
+  _exec = (command) => {
+    return Promise((resolve, reject) => {
+      // For some reason, stderr contains git's output.
+      exec(command, (error, stdout, stderr) => {
+        if (error) {
+          reject(error.message)
+        }
+
+        resolve({stdout, stderr})
+      })
+    })
+  }
 }
+
+module.exports = GitTree
